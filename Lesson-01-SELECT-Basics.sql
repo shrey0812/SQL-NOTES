46a363c75433484a95b087e7f09340d351874460
Shrey Gupta <shreygupta0812@gmail.com>
2025-06-14 13:25:20 +0530

    Update Lesson-01-SELECT-Basics.sql

--- Lesson-01-SELECT-Basics.sql
+++ Lesson-01-SELECT-Basics.sql
@@ -1,20 +1,31 @@
--- Lesson 01: SELECT Basics
--- 📘 Introduction to SELECT queries: Retrieving data from SQL tables
-
--- 🔹 Task 1: Find the title of each film
+-- 📘 Lesson 01: SELECT Basics
+-- 🔍 Task 1: Find the title of each film
 SELECT title FROM movies;
 
--- 🔹 Task 2: Find the director of each film
+-- 🔍 Task 2: Find the director of each film
 SELECT director FROM movies;
 
--- 🔹 Task 3: Find the title and director of each film
+-- 🔍 Task 3: Find the title and director of each film
 SELECT title, director FROM movies;
 
--- 🔹 Task 4: Find the title and year of each film
+-- 🔍 Task 4: Find the title and year of each film
 SELECT title, year FROM movies;
 
--- 🔹 Task 5: Find all the information about each film
+-- 🔍 Task 5: Find all the information about each film
 SELECT * FROM movies;
 
--- ✅ Each query demonstrates a different way to extract specific data from the 'movies' table
--- using SELECT statements with column lists or the asterisk (*) shorthand.
+-- 🔬 Experiment Zone - Shrey's Custom Queries
+-- Q1: Get only the title and length
+SELECT title, length_minutes FROM movies;
+
+-- Q2: Movies released before 2005
+SELECT * FROM movies WHERE year < 2005;
+
+-- Q3: Movies sorted by year (desc)
+SELECT * FROM movies ORDER BY year DESC;
+
+-- Q4: Movies with more than 100 minutes
+SELECT title FROM movies WHERE length_minutes > 100;
+
+-- Q5: Top 2 shortest movies
+SELECT * FROM movies ORDER BY length_minutes ASC LIMIT 2;
